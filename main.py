bbab847003905a7ce2f936bf39d291b360c5a14e
SomeSoriyama <soriyama@proton.me>
2024-12-18 15:48:46 +0300

    Added uvicorn instance at running main

--- main.py
+++ main.py
@@ -7,16 +7,21 @@ from router import router as tasks_router
 
 
 @asynccontextmanager
-async def lifespan(current_app: FastAPI):
+async def lifespan(_: FastAPI):
     print('Старт')
     await delete_tables()
     print('База очищена')
     await create_tables()
     print('База готова к работе')
-    yield current_app
+    yield
     print('Выключение')
 
 
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(tasks_router)
+
+
 if __name__ == '__main__':
-    app = FastAPI(lifespan=lifespan)
-    app.include_router(tasks_router)
+    from uvicorn import run
+    run(app, port=80, log_level="info")
